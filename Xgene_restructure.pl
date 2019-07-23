1b80c334331fe5a3d8a31016666ccdf8e385082f
Yuan Olivia ZHU <30611503+YuanOZhu@users.noreply.github.com>
2019-07-23 16:14:41 +0800

    Update and rename 02_Xgene_restructure.pl to Xgene_restructure.pl

--- Xgene_restructure.pl
+++ Xgene_restructure.pl
@@ -1,6 +1,10 @@
 use warnings;
 use strict;
 use Data::Dumper;
+
+##Author: ZHU O. Yuan (Genome Institute of Singapore)
+##Script name: Xgene_restructure.pl
+##Usage: perl Xgene_restructure.pl aligned.fasta > x-gene.fa
 #this script extracts the first 18bp of the reference and attaches it to the end so the x gene sequence is complete
 
 open(my$fi,$ARGV[0]) or die;
