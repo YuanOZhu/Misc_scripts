2d6c7389200374bcb76040780c15367d880bbe73
Yuan Olivia ZHU <30611503+YuanOZhu@users.noreply.github.com>
2019-07-23 16:25:41 +0800

    Update and rename 04_sfs_sorted.pl to Sfs_sorted.pl

--- Sfs_sorted.pl
+++ Sfs_sorted.pl
@@ -1,10 +1,17 @@
 use warnings;
 use strict;
 use Data::Dumper;
-###summarizes variable sites only ignoring missing -
+
+##Author: ZHU O. Yuan (Genome Institute of Singapore)
+##Script name: Sfs_sorted.pl
+##Usage: perl Sfs_sorted.pl aligned.fa > sfs.out
+#This script summarizes and outputs counts of all observed bases for variable sites only
 ##perl 03_sfs_sorted.pl inputRaxML.phy.reduced.nuh19.fasta > 03_sfs_sorted.nuh19.out
 
+#array of hashes to store counts of each base at every position
 my@pileup=();
+
+#add counts of seen bases for each sequence at each position
 open(my$fi,$ARGV[0]) or die;
 while(my$line=<$fi>){
     chomp $line;
@@ -22,6 +29,7 @@ while(my$line=<$fi>){
     }
 }close($fi);
 
+#for every position, sort by most frequent observed base and print out
 for(my$c=0;$c<@pileup;$c++){
     my$pos=$c+1;
     print "$pos";
