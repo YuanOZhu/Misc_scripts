3ff6fe9968d770e4ae1ae8ed16d56558f0caf432
Yuan Olivia ZHU <30611503+YuanOZhu@users.noreply.github.com>
2019-07-23 16:34:05 +0800

    Update and rename reformat_reference.pl to Reformat_reference.pl

--- Reformat_reference.pl
+++ Reformat_reference.pl
@@ -1,19 +1,19 @@
 use warnings;
 use strict;
 use Data::Dumper;
-#perl reformat_reference.pl GenoBGenoCsubtypes_geno8.fa > reformat_reference.out
 
-#my%list=("|ref_HBVB_non-ambiguous|"=>1,"AY033073NetherlandsB4"=>1,"AY033072NetherlandsB4"=>1);
+##Author: ZHU O. Yuan (Genome Institute of Singapore)
+##Script name: Reformat_reference.pl
+##Usage: perl reformat_reference.pl GenoBGenoCsubtypes_geno8.fa > reformat_reference.out
+#hardcoded script to reformat reference sequences from NCBI as needed 
+
 my$strain="";
 open(my$fi,$ARGV[0]) or die;
 while(my$line=<$fi>){
     chomp $line;
     if($line =~ /^>/){$strain=$line;}
     else{
-#	if(!defined $list{$strain}){
 	    my$new=substr($line,1395,length($line)-1396).substr($line,0,1396);
 	    print "$strain\n$new\n";
-#	}
-#	else{print "$strain\n$line\n";}
     }
 }close($fi);
