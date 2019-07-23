56b57cd377f091fbf894a413bec43debc6f021bf
Yuan Olivia ZHU <30611503+YuanOZhu@users.noreply.github.com>
2019-07-23 16:28:23 +0800

    Update and rename 05_pairwise_diff.pl to Pairwise_diff.pl

--- Pairwise_diff.pl
+++ Pairwise_diff.pl
@@ -1,8 +1,13 @@
 use warnings;
 use strict;
 use Data::Dumper;
-#counts base differences between any 2 sequences ignoring -
 
+##Author: ZHU O. Yuan (Genome Institute of Singapore)
+##Script name: Pairwise_diff.pl
+##Usage: perl Pairwise_diff.pl samples.fa missing_character > out.txt
+#This script counts base differences between any 2 sequences ignoring -/N as specified
+
+#stores all sequences in array
 my@strains=();my%sequences=();my$strain="";
 open(my$fj,$ARGV[0]) or die;
 while(my$line=<$fj>){
@@ -11,6 +16,8 @@ while(my$line=<$fj>){
     else{$sequences{$strain}=$line;push(@strains,$strain);}
 }close($fj);
 
+#compare every pair of sequences for count of different bases
+#print as % difference
 for(my$s=0;$s<@strains;$s++){
     for(my$t=$s+1;$t<@strains;$t++){
 	my@seq1=split('',$sequences{$strains[$s]});
@@ -19,7 +26,7 @@ for(my$s=0;$s<@strains;$s++){
 	my$diff=0;
 	for(my$a=0;$a<@seq1;$a++){
 	    my$pos=$a+1;
-	    if(($seq1[$a] ne "-")&&($seq2[$a] ne "-")){
+	    if(($seq1[$a] ne $ARGV[1])&&($seq2[$a] ne $ARGV[1])){
 		if($seq1[$a] ne $seq2[$a]){
 		    $diff++;
 		}
